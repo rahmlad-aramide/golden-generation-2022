dbf2d4c6d63a0d860274031bf92e25eb20feda15
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-03-11 14:47:58 +0100

    new

--- regform/individual-regform/js/main.js
+++ regform/individual-regform/js/main.js
@@ -1,7 +1,29 @@
-(function($) {
+;(function ($) {
+  $('#reset').on('click', function () {
+    $('#register-form').reset()
+  })
+})(jQuery)
 
-  $('#reset').on('click', function(){
-      $('#register-form').reset();
-  });
+// let surnameValue
+// document.querySelector('#surname').addEventListener('change', function (e) {
+//   console.log(e.target.value)
+//   surnameValue = e.target.value
+// })
 
-})(jQuery);
+function getInputBy(id) {
+  let surnameValue
+  document.querySelector(id).addEventListener('change', function (e) {
+    // console.log(e.target.value)
+    surnameValue = e.target.value
+  })
+  return { surnameValue }
+}
+console.log(getInputBy('#surname').surnameValue)
+
+function getSurname() {
+  console.log({ surnameValue })
+  // surname.onchange = function (e) {
+  //   console.log(e.target.value)
+  // }
+}
+getSurname()
