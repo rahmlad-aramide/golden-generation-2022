38816d27f988252924e28f768c4a3fba5a8ceecf
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-03-12 19:55:15 +0100

    addJs

--- regform/individual-regform/js/main.js
+++ regform/individual-regform/js/main.js
@@ -4,26 +4,76 @@
   })
 })(jQuery)
 
-// let surnameValue
-// document.querySelector('#surname').addEventListener('change', function (e) {
-//   console.log(e.target.value)
-//   surnameValue = e.target.value
-// })
+let surnameValue
+document.querySelector('#surname').addEventListener('change', function (e) {
+  console.log(e.target.value)
+  surnameValue = e.target.value
+})
 
-function getInputBy(id) {
-  let surnameValue
-  document.querySelector(id).addEventListener('change', function (e) {
-    // console.log(e.target.value)
-    surnameValue = e.target.value
-  })
-  return { surnameValue }
-}
-console.log(getInputBy('#surname').surnameValue)
+let othernameValue
+document.querySelector('#other_name').addEventListener('change', function (e) {
+  console.log(e.target.value)
+  othernameValue = e.target.value
+})
 
-function getSurname() {
-  console.log({ surnameValue })
-  // surname.onchange = function (e) {
-  //   console.log(e.target.value)
-  // }
+let phoneValue
+document.querySelector('#phone').addEventListener('change', function (e) {
+  console.log(e.target.value)
+  phoneValue = e.target.value
+})
+
+let emailValue
+document.querySelector('#email').addEventListener('change', function (e) {
+  console.log(e.target.value)
+  emailValue = e.target.value
+})
+
+let schoolValue
+document.querySelector('#school').addEventListener('change', function (e) {
+  console.log(e.target.value)
+  schoolValue = e.target.value
+})
+
+let homeAddressValue
+document.querySelector('#address').addEventListener('change', function (e) {
+  console.log(e.target.value)
+  homeAddressValue = e.target.value
+})
+
+let maleInputValue
+document.querySelector('#male').addEventListener('click', function (e) {
+  console.log(e.target.value)
+  maleInputValue = e.target.value
+})
+
+let femaleInputValue
+document.querySelector('#female').addEventListener('click', function (e) {
+  console.log(e.target.value)
+  femaleInputValue = e.target.value
+})
+
+let selectValue
+document.querySelector('#class').addEventListener('click', function (e) {
+  console.log(e.target.value)
+  selectValue = e.target.value
+})
+
+const data = { surnameValue }
+
+const fetchData = {
+  method: 'POST',
+  body: JSON.stringify(data),
+  headers: new Headers({
+    'Content-Type': 'application/json; charset=UTF-8',
+  }),
 }
-getSurname()
+
+function handleSubmit() {
+  fetch(url, fetchData)
+    .then((res) => {
+      console.log({ res })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
